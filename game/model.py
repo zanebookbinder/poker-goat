26f144ba1c12d14736564cd73e4a8ff08d062545
Zane Bookbinder <zanemb86@gmail.com>
2023-11-01 08:39:33 -0400

    Made model train correctly
    Co-authored-by: Rahul Dasgupta <rahuldasgupta-24@users.noreply.github.com>

--- game/model.py
+++ game/model.py
@@ -1,9 +1,9 @@
 import numpy as np
-from sklearn import preprocessing
 import random
 from keras.models import Sequential
 from keras.layers import Dense
 from utils import readExperiencesFile
+import tensorflow as tf
 
 class Model():
     def __init__(self, model=None):
@@ -24,24 +24,6 @@ class Model():
     
         return int(np.argmax(bestAction))
 
-    # def getModelInput(self, player, sharedCards):
-    #     playerHandCards = [0] * 13
-    #     suitCounts = collections.defaultdict(int)
-
-    #     for card in player.hand.cards:
-    #         cardRank = card.rank
-    #         playerHandCards[cardRank-2] += 1
-    #         suitCounts[card.suit] += 1
-
-    #     commonCards = [0] * 13
-    #     for card in sharedCards:
-    #         cardRank = card.rank
-    #         commonCards[cardRank-2] += 1
-    #         suitCounts[card.suit] += 1
-
-    #     suitMode = max(suitCounts.values())
-    #     return [self.round] + playerHandCards + commonCards + [suitMode]
-
     def createModel(self):
         model = Sequential()
         model.add(Dense(64, input_dim=30, activation= 'sigmoid'))
@@ -61,14 +43,15 @@ class Model():
         experiences = readExperiencesFile()
 
         # with replacement???
-        samples = [random.choice(experiences) for _ in range(10)]
-        
-        X = []
-        YTarget = []
-        for sample in samples:
+        sample_size = 10
+        samples = [random.choice(experiences) for _ in range(sample_size)]
+
+        x = np.empty((sample_size, 30))
+        YTarget = np.empty((sample_size, 3))
+        for i, sample in enumerate(samples):
             state, action, nextState, reward = sample
-            state = np.array(state).reshape(1, -1)
-            X.append(state)
+            x[i] = np.array(state)
+            state2D = np.array(state).reshape(1, -1)
             if not nextState:
                 Qmax = 0
             else:
@@ -76,18 +59,14 @@ class Model():
                 QPrimeOutput = self.model.predict(nextState)[0]
                 Qmax = max(QPrimeOutput)
             
-            prediction = self.model.predict(state)[0]
+            prediction = self.model.predict(state2D)[0]
             y_actual = prediction.copy()
+            y_actual[action] = Qmax + reward
+            
+            YTarget[i] = y_actual
 
-            y_actual[action] = Qmax + reward #normalize this??
-            y_actual = preprocessing.normalize([y_actual])
-            YTarget.append(np.array(y_actual).reshape(1, -1)) 
-
-        print(X)
+        print(x)
         print('----')
         print(YTarget)
 
-        # self.model.fit(X, YTarget)
-        self.model.train_on_batch(X, YTarget)
-            
-            
+        self.model.fit(x, YTarget)            
