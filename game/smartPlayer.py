5338158059a38e6d7f3b697f13b2864a0d95248e
Zane Bookbinder <zanemb86@gmail.com>
2023-11-30 09:34:05 -0500

    Made smart player probabilistic and able to handle hole cards only

--- game/smartPlayer.py
+++ game/smartPlayer.py
@@ -1,18 +1,30 @@
 from player import Player
 from handScoreUtil import scoreHand
+import numpy as np
+from handRankUtil import calculateSimpleHandValue
 
 class SmartPlayer(Player):
 	def __init__(self, name, index, chipCount):
 		super().__init__(name, index, chipCount)
 
 	def chooseBestAction(self, model, newGameExperience, commonCards):
-		handScore = scoreHand(self.hand.cards, commonCards)
+		if commonCards:
+			handScore = scoreHand(self.hand.cards, commonCards)
+		else:
+			handScore = calculateSimpleHandValue(self.hand.cards)
+			handScore = (handScore + 1) / 2
 
-		if handScore > 0.5:
-			return 2
-		
-		if handScore > 0.2:
-			return 1
-		
-		return 0
+		actions = [0, 1, 2]
+
+		if handScore > 0.6:
+			probs = [0.1, 0.2, 0.7]
+		elif handScore > 0.3:
+			probs = [0.2, 0.6, 0.2]
+		else:
+			probs = [0.7, 0.2, 0.1]
+
+		return np.random.choice(
+			actions,
+			p = probs
+		)
 
