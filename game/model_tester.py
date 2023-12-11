bdb7d793e747e2af1054ef9e36ed5cfcb609ceff
Zane Bookbinder <zanemb86@gmail.com>
2023-12-11 09:35:54 -0500

    Fixed model tester for auto encoder

--- game/model_tester.py
+++ game/model_tester.py
@@ -34,26 +34,41 @@ four_aces = [
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0	#CommonCards
 ]
 
-def test_model(model):
-	qVals = model.predict(horrible_cards,verbose=0)[0]
+def test_model(model, autoencoder):
+	modelInput1 = autoencoder.predict([horrible_cards[:52]], verbose=0)[0]
+	modelInput2 = autoencoder.predict([horrible_cards[52:]], verbose=0)[0]
+	modelInput = modelInput1.tolist() + modelInput2.tolist()
+	qVals = model.predict([modelInput],verbose=0)[0]
 	print('With horrible cards:', qVals)
-	qVals = model.predict(four_aces, verbose=0)[0]
+
+	modelInput1 = autoencoder.predict([four_aces[:52]], verbose=0)[0]
+	modelInput2 = autoencoder.predict([four_aces[52:]], verbose=0)[0]
+	modelInput = modelInput1.tolist() + modelInput2.tolist()
+	qVals = model.predict([modelInput],verbose=0)[0]
 	print('With four aces:', qVals)
 
-# import os
-# from keras.models import load_model
+import os
+from keras.models import load_model, Model
 
-# model_dir = '/Users/zanebookbinder/Desktop/poker-goat/game/december_6th'
-# model_files = [f for f in os.listdir(model_dir) if f.endswith('.keras')]
-# model_files.sort(key = lambda x: int(x.replace('model_', '').replace('.keras', '')))
+model_dir = '/Users/zanebookbinder/Desktop/poker-goat/game/december_10th'
+model_files = [f for f in os.listdir(model_dir) if f.endswith('.keras')]
+model_files.sort(key = lambda x: int(x.replace('model_', '').replace('.keras', '')))
+
+# Load the highest-verion model
+largest_model_file = model_files[-1]
+print(largest_model_file)
+
+autoencoder_dir = '/Users/zanebookbinder/Desktop/poker-goat/game/autoencoder'
+autoencoder_files = [f for f in os.listdir(autoencoder_dir) if f.endswith('.keras')]
+autoencoder_files.sort(key = lambda x: int(x.replace('model_', '').replace('.keras', '')))
+largest_ae_file = autoencoder_files[-1]
 
 
-# # Load the highest-verion model
-# largest_model_file = model_files[-1]
-# print(largest_model_file)
-# model = load_model(os.path.join(model_dir, largest_model_file))
+model = load_model(os.path.join(model_dir, largest_model_file))
+autoencoder = load_model(os.path.join(autoencoder_dir, largest_ae_file))
+autoencoder =  Model(inputs=autoencoder.input, outputs=autoencoder.get_layer('dense_1').output)
 
-# test_model(model)
+test_model(model, autoencoder)
 
 
 
